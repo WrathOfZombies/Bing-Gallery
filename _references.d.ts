217e6940aad8abc384e19d9a9dd1dc6583f505b8
Bhargav Krishna <bhargav.krishna@outlook.com>
2015-12-17 18:35:01 -0800

    Modifying Cordova launch and bootstrapping mode

--- _references.d.ts
+++ _references.d.ts
@@ -4,9 +4,9 @@
 
 /// <reference path="src/ts/core/models/bing.image.ts" />
 
-/// <reference path="src/ts/utils/constants/regions.ts" />
-
 /// <reference path="src/ts/core/services/bing.service.ts" />
+
+/// <reference path="src/ts/utils/constants/regions.ts" />
 
 /// <reference path="src/ts/utils/types/enumerations.ts" />
 
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,94 +1,79 @@
 /// <reference path="../../_references.d.ts" />
 /// <reference path="../../typings/tsd.d.ts" />
 
-/**
- * Creates a namespace for all the functional logic to fall within a closure. 
- */
 namespace BingGallery.Core {
+    export function resolvePath(path: string): string {
+        return '/www/templates/' + path + '.html';
+    }
 
-	/**
-	* Helper to find html template paths. Can be invoked using BingGallery.resolvePath()
-	*/
-	export function resolvePath(path: string): string {
-		return '/www/templates/' + path + '.html';
-	}
+    export let module = angular.module('BingGallery', ['ngAnimate', 'ngMaterial', 'ui.router']);
 
-	/**
-	 * Angular module construction. Resuse this throught your app if necessary, although the 
-	 * module class below should perform all your registrations in one place. Opinionated 
-	 * thought that its better that way.
-	 */
-	export let module = angular.module('BingGallery', ['ngAnimate', 'ngMaterial', 'ui.router']);
+    class App {
+        private registerComponents() {
+            this.module.factory('BingImageService', ['$http', ($http: ng.IHttpService) => { return new BingGallery.Core.Services.BingImageService($http); }]);
+        }
 
-	/**
-	 * Module class that manages bootstrapping and module registration and configuration.
-	 */
-	class App {
-		private registerComponents() {
-			this.module.factory('BingImageService', ['$http', ($http: ng.IHttpService) => { return new BingGallery.Core.Services.BingImageService($http); }]);
-		}
+        private registerStates(
+            $stateProvider: ng.ui.IStateProvider,
+            $locationProvider: ng.ILocationProvider
+        ) {
+            $stateProvider.state('home', {
+                templateUrl: resolvePath('views/home')
+            });
 
-		private registerStates(
-			$stateProvider: ng.ui.IStateProvider,
-			$locationProvider: ng.ILocationProvider
-		) {
-			$stateProvider.state('home', {
-				templateUrl: resolvePath('views/home')
-			});
+            $locationProvider.html5Mode(true).hashPrefix('!');
+        }
 
-			$locationProvider.html5Mode(true).hashPrefix('!');
-		}
+        private configureComponents() {
+            this.module.config(['$stateProvider', '$locationProvider', this.registerStates]);
+        }
 
-		private configureComponents() {
-			this.module.config(['$stateProvider', '$locationProvider', this.registerStates]);
-		}
+        constructor(private module: ng.IModule) {
+            console.log('Creating');
+            this.registerComponents();
+            this.configureComponents();
+            this.run();
+        }
 
-		constructor(private module: ng.IModule) {
-			this.registerComponents();
-			this.configureComponents();
-		}
+        run() {
+            this.module.run(['$state', 'BingImageService', ($state: ng.ui.IStateService, BingImageService: BingGallery.Core.Services.BingImageService) => {
+                $state.go('home');
+                console.log(BingImageService);
+                BingImageService.getImageFromCalendar();
+            }]);
+        }
+    }
 
-		run(params: Array<any>) { this.module.run(params); }
-	}
+    function bootstrap() {
+        if (!(document && angular)) return;
 
-	new App(module).run(['$state', 'BingImageService', ($state: ng.ui.IStateService, BingImageService: BingGallery.Core.Services.BingImageService) => {
-		$state.go('home');
-		console.log(BingImageService);
-		BingImageService.getImageFromCalendar();
-	}]);
+        new App(module);
+
+        angular.element(document).ready(() => {
+            console.log('Bootstrapping');
+            angular.bootstrap(document, ['BingGallery']);
+        });
+    }
+
+    export function startApplication() {
+        var context = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
+        var windowsMode = window.hasOwnProperty('Windows');
+        if (context && !windowsMode) {
+            console.log('cordova mode: waiting for device');
+            document.addEventListener('deviceready', () => {
+                console.log('device ready');
+                bootstrap();
+            }, false);
+        }
+
+        if (windowsMode) {
+            console.log('windows mode: waiting for device');
+        }
+        else {
+            console.log('web mode: waiting for browser');
+        }
+        bootstrap();
+    }
 }
 
-//// For an introduction to the Blank template, see the following documentation:
-//// http://go.microsoft.com/fwlink/?LinkID=397705
-//// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-//// and then run "window.location.reload()" in the JavaScript Console.
-//module Cordova {
-//    "use strict";
-
-//    export module Application {
-//        export function initialize() {
-//            document.addEventListener('deviceready', onDeviceReady, false);
-//        }
-
-//        function onDeviceReady() {
-//            // Handle the Cordova pause and resume events
-//            document.addEventListener('pause', onPause, false);
-//            document.addEventListener('resume', onResume, false);
-
-//            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-//        }
-
-//        function onPause() {
-//            // TODO: This application has been suspended. Save application state here.
-//        }
-
-//        function onResume() {
-//            // TODO: This application has been reactivated. Restore application state here.
-//        }
-
-//    }
-
-//    window.onload = function () {
-//        Application.initialize();
-//    }
-//}
+BingGallery.Core.startApplication();
