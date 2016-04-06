13d1aa46f2a956ff241994a72635ef3c325c32cd
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-04-05 22:26:54 -0700

    Beginning migration to Angular2

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,6 @@
 
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
-    bower = require('gulp-bower'),
-    mainBowerFiles = require('main-bower-files'),
-    filter = require('gulp-filter'),
     del = require('del'),
 
     sass = require('gulp-sass'),
@@ -16,8 +13,9 @@ var gulp = require('gulp'),
     tsConfigGlob = require('tsconfig-glob'),
     merge = require('merge2'),
 
-    connect = require('gulp-connect'),
+    webserver = require('gulp-webserver'),
     config = require('./gulpfile.config.json'),
+    packageConfig = require('./package.json'),
     errorHandler = function (error) {
         console.log(error);
         this.emit('end');
@@ -26,31 +24,49 @@ var gulp = require('gulp'),
 var tsProject = typescript.createProject('./tsconfig.json', { sortdest: true });
 
 gulp.task('clean', function (done) {
-    return del([config.lib.source, config.lib.dest, config.app.dest], done);
+    return del(config.app.dest, done);
 });
 
-gulp.task('bower:download', ['clean'], function () {
-    return bower();
+gulp.task('ref', function () {
+    return tsConfigGlob();
 });
 
-gulp.task('bower:install', ['bower:download'], function () {
-    return gulp.src(mainBowerFiles({ base: config.lib.source }))
-        .pipe(gulp.dest(config.lib.dest));
+gulp.task('install', ['clean', 'ref'], function (done) {
+    return Object.keys(packageConfig.overrides)
+        .map(function (libraryName) {
+            try {
+                var library = packageConfig.overrides[libraryName];
+                if (Array.isArray(library)) {
+                    var files = library.map(function (filePath) {
+                        console.log('Copied: ', filePath);
+                        return config.lib.source + "/" + libraryName + "/" + filePath;
+                    });
+                    return gulp.src(files)
+                        .pipe(plumber(errorHandler))
+                        .pipe(gulp.dest(config.lib.dest));
+                }
+                else {
+                    var file = config.lib.source + "/" + libraryName + "/" + library;
+                    console.log('Copied: ', library);
+                    return gulp.src(file)
+                        .pipe(plumber(errorHandler))
+                        .pipe(gulp.dest(config.lib.dest));
+                }
+            }
+            catch (exception) {
+                console.error('Failed to load package: ', libraryName);
+            }
+        });
 });
 
 gulp.task('compile:sass', function () {
     return gulp.src(config.app.source + "/**/*.scss")
         .pipe(plumber(errorHandler))
         .pipe(sass())
-        .pipe(gulp.dest(config.app.dest))
-        .pipe(connect.reload());
+        .pipe(gulp.dest(config.app.dest));
 });
 
-gulp.task('generate-references', function () {
-    return tsConfigGlob();
-});
-
-gulp.task('compile:ts', ['generate-references'], function () {
+gulp.task('compile:ts', function () {
     var tsResult = tsProject.src()
         .pipe(sourcemaps.init())
         .pipe(plumber(errorHandler))
@@ -58,48 +74,34 @@ gulp.task('compile:ts', ['generate-references'], function () {
 
     tsResult.js
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(config.app.dest))
-        .pipe(connect.reload());
-});
-
-gulp.task('copy', function () {
-    gulp.src([
-        "!" + config.app.source + "/**/*.scss",
-        "!" + config.app.source + "/**/*.ts",
-        config.app.source + "/**/*.*"
-    ], { base: config.app.source })
-        .pipe(plumber(errorHandler))
         .pipe(gulp.dest(config.app.dest));
 });
 
-gulp.task('serve', ['compile:sass', 'compile:ts', 'copy'], function () {
-    connect.server({
-        root: config.server.root,
-        host: config.server.host,
-        port: config.server.port,
-        fallback: config.server.fallback,
-        livereload: true
-    });
-});
-
-gulp.task('server:reload', ['copy'], function () {
-    gulp.src([
-        "!" + config.app.source + "/**/*.scss",
-        "!" + config.app.source + "/**/*.ts",
-        config.app.source + "/**/*.*"
-    ])
-        .pipe(connect.reload())
-        .pipe(plumber(errorHandler));
+gulp.task('copy', function () {
+    gulp.src(config.app.source + "/**/!(*.ts|*.scss)", { base: config.app.source })
+        .pipe(plumber(errorHandler))
+        .pipe(gulp.dest(config.app.dest));
 });
 
 gulp.task('watch', function () {
     gulp.watch(config.app.source + "/**/*.scss", ['compile:sass']);
     gulp.watch(config.app.source + "/**/*.ts", ['compile:ts']);
-    gulp.watch([
-        "!" + config.app.source + "/**/*.scss",
-        "!" + config.app.source + "/**/*.ts",
-        config.app.source + "/**/*.*"
-    ], ['server:reload']);
+    gulp.watch(config.app.source + "/**/!(*.ts|*.scss)", ['copy']);
 });
 
-gulp.task('default', ['serve', 'watch']);
+gulp.task('build', ['compile:sass', 'compile:ts', 'copy']);
+gulp.task('default', ['build', 'watch'], function () {
+    return gulp.src(config.app.dest)
+        .pipe(plumber(errorHandler))
+        .pipe(webserver({
+            host: config.server.host,
+            port: config.server.port,
+            directoryListing: config.server.directoryListing,
+            https: config.server.https,
+            livereload: {
+                enable: true,
+                liveCSS: true
+            },
+            fallback: config.server.fallback
+        }));
+});
---
+++ src/system.config.ts
@@ -0,0 +1,40 @@
+﻿System.config({
+    packages: {
+        'app': {
+            format: 'register',
+            meta: {
+                'bootstrap.js': {
+                    format: 'register',
+                    deps: [
+                        'lib/underscore'
+                    ]
+                }
+            }
+        },
+        'lib': {
+            format: 'global',
+            meta: {
+                'lib/angular2.dev.js': {
+                    deps: [
+                        'lib/Rx',
+                        'lib/angular2-polyfills',
+                    ]
+                },
+                'lib/router.dev.js': {
+                    deps: ['lib/angular2.dev']
+                },
+                'lib/http.dev.js': {
+                    deps: ['lib/angular2.dev']
+                },
+                'lib/jquery.fabric.js': {
+                    exports: '$'
+                },
+                'lib/underscore.js': {
+                    exports: '_'
+                },                
+            }
+        }
+    }
+});
+
+System.import('app/bootstrap').then(null, console.error.bind(console));
